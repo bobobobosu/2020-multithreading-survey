d2d3f40fb9ea4ec490166f4c9689ff2e7f7553e9
ubuntu <aaa@xyz.com>
2021-11-09 12:18:31 -0800

    fix

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.use(express.static(__dirname)); // Current directory is root
+app.use(function(req, res, next) {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'src'))); //  "public" off of current is root
 
 app.listen(8080);
